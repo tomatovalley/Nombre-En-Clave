b3ee8993a3e8104cea55cd5e4154ac82ba2e5fa7
JesusfGarcia <jesusfgarciavaldez@gmail.com>
2018-12-11 12:16:46 -0700

    prototipo de creación de tablero

---
+++ app/src/main/java/com/example/jesus/codenames/Play.kt
@@ -0,0 +1,38 @@
+package com.example.jesus.codenames
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_play.*
+import kotlin.random.Random
+
+class Play : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //val tablero = generar_tablero()
+        setContentView(R.layout.activity_play)
+
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+
+
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+    }
+
+
+
+
+}
+
+
+
