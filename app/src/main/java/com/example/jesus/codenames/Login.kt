fab113bc6fcd4ddeea7f704ced2a974a8bc23539
JesusfGarcia <jesusfgarciavaldez@gmail.com>
2019-01-17 22:49:57 -0700

    agregamos firebase a la app, y creamos la pantalla login

---
+++ app/src/main/java/com/example/jesus/codenames/Login.kt
@@ -0,0 +1,16 @@
+package com.example.jesus.codenames
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.google.firebase.database.FirebaseDatabase
+
+class Login : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+
+    }
+
+}
